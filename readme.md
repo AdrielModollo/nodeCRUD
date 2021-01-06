3c36c13b566af944991cfc9de5939977d36ea040
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-05 23:31:52 -0300

    Instalando e configurando nodemon

--- readme.md
+++ readme.md
@@ -4,4 +4,8 @@ npm init
 
 npm install express --save 
 
-npm install ejs --save
+npm install ejs --save <- biblioteca para renderização do html
+
+npm install nodemon --save-dev <- auxilia ao reiniciar o servidor automaticamente sempre que feito uma alteração
+npm run dev <- para acionar nosso comando dev dentro de nosso package.json
+nodemon server.js
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express') 
 const app = express()
 
 app.listen(3000, function() {
     console.log('server rodando na porta 3000')
-})
+}) //indica que nosso servidor está rodando na porta 3000
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') //chama nossa biblioteca ejs
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
-})
+}) //faz leitura de nosso dados
