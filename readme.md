b47652fc2c0f375c2989a6bdc6c91036c3e85bcf
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-06 01:36:26 -0300

    Adicionado inserção em banco de dados MongoDB

--- readme.md
+++ readme.md
@@ -13,3 +13,8 @@ nodemon server.js
 npm install body-parser --save  <- middleware como body-parser, são basicamente plugins que alteram o objeto request ou response antes de serem manipulados pelo nosso aplicativo.
 
 O método urlencoded dentro de body-parser diz ao body-parser para extrair dados do elemento form e adicioná-los à propriedade body no objeto request. Faça o teste, dê um console.loge preencha o formulário, você deverá ver tudo no campo de formulário dentro do objeto
+
+
+# Banco de dados
+
+npm install mongodb --save 
--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Adriel:5aGGcwrUAnhnFdQo@cluster0.7vary.mongodb.net/crudNode?retryWrites=true&w=majority";
+
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.listen(3000, function() {
-    console.log('server rodando na porta 3000')
-}) //indica que nosso servidor está rodando na porta 3000
+const client = new MongoClient(uri, { useNewUrlParser: true });
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('crudNode') // coloque o nome do seu DB
+  
+    app.listen(3000, () => {
+      console.log('Server running on port 3000')
+    })
+  })
+
+
 
 app.set('view engine', 'ejs') //chama nossa biblioteca ejs
 
@@ -15,5 +27,10 @@ app.get('/', (req, res) => {
 }) //faz leitura de nosso dados
 
 app.post('/show', (req, res) => {
-    console.log(req.body)
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+    console.log('salvo no banco de dados')
+    res.redirect('/')
+    })
 })
