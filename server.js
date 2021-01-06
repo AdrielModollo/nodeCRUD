a18dbc19dab5ee1166b63bbbde48d6e75ee7e8ab
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-05 23:11:56 -0300

    Configurando servidor com navegador

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("A Força está com você!")
+const express = require('express')
+const app = express()
+
+app.listen(3000, function() {
+    console.log('server rodando na porta 3000')
+})
